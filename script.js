d66d88020088d765fbc5b25d19686dc18ff1513f
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2021-11-23 21:09:02 +0000

    Fixed script.js

--- script.js
+++ script.js
@@ -1,66 +1,44 @@
 //All the survey responses are stored in a list called "people". (Remember to use dot or [ ] syntax!)
 
 // 1. Print out the name of the first person who responded to the survey. You should see informatio nabout Daniel
-console.log(people[0])
 
 
 
 // 2. Print out Brenton's second favorite band. You should see "The Format"
-console.log(people[7]['Favorite Bands'][1])
+
 
 
 
 // 3. For each person on this list, print out a descriptor of where they live in the form "____ lives in _____."
-for(var i = 0; i < people.length; i++){
-	console.log(`${people[i].Name} lives in ${people[i].State}`)
-}
+
 
 
 
 // 4. Print out each person's nickname. Write some control flow that determines whether / how to respond if someone didn't provide a nickname.
-for(var i = 0; i < people.length; i++){
-	if(people[i].Nickname != ""){
-		console.log(people[i].Nickname)
-	}
-	else {
-		console.log(`${people[i].Name} has no nickname`)
-	}
-}
+
+
+
 
 
 // 5. Find the average of the respondents' ages.
-var sum = 0
-for(var i = 0; i < people.length; i++){
-	sum += people[i].Age
-}
-console.log(sum/people.length)
+
+
 
 
 // 6. Print out the percentage of respondents who have brown hair.
-var brownSum = 0;
-for(var i = 0; i < people.length; i++){
-	if(people[i]["Hair Color"] == "Brown"){
-		brownSum++;
-	}
-}
-console.log(brownSum/people.length)
+
+
+
 
 
 
 // 7. Create two new lists - one for programmers, and one for non programmers.
 //    Sort all the people in the main list into these two more specific lists.
-var programmer = []
-var nonprogrammers = []
-for(var i = 0; i < people.length; i++){
-	if(people[i]["Is Programmer"]){
-		programmer.push(people[i].Name)
-	}
-	else {
-		nonprogrammers.push(people[i].Name)
-	}
-}
-console.log(programmer)
-console.log(nonprogrammers)
+
+
+
+
+
 
 
 // 8. Iterate over the list in order to figure out how many respondents listed "The Office" as one of their favorite shows.
